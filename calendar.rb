2df189325fc4a311bd22b37415143be1d5e50af2
koko_u <kozaki.tsuneaki@gmail.com>
2011-01-29 20:36:56 +0900

    each_week と each_day を実装

--- calendar.rb
+++ calendar.rb
@@ -11,6 +11,12 @@ class Calendar
     (@first_day >> 1) - 1
   end
 
+  def first_sunday
+    ret = first_day
+    ret -= 1 until ret.sunday?
+    ret
+  end
+
   def last_saturday
     lsaturday = last_day
     lsaturday += 1 until lsaturday.saturday?
@@ -18,6 +24,19 @@ class Calendar
   end
 
   def each_week
-    (last_day.day / 7 + 1)
+    week = Array.new(7) { |i| first_sunday + i }
+    until week[0] > last_saturday
+      yield week
+      week.map! { |i| i + 7 }
+    end
+    self
   end
+
+  def each_day
+    first_day.upto(last_day) do |date|
+      yield date
+    end
+    self
+  end
+
 end
--- calendar_test.rb
+++ calendar_test.rb
@@ -21,25 +21,57 @@ class CalendarTest < Test::Unit::TestCase
     assert_equal Date.new(2012, 2, 29), feb.last_day
   end
 
+  def test_first_sunday
+    jan = Calendar.new(2011, 1)
+    assert_equal Date.new(2010, 12, 26), jan.first_sunday
+  end
+
   def test_last_saturday
     jan = Calendar.new(2011, 1)
     assert_equal Date.new(2011, 2, 5), jan.last_saturday
   end
 
   def test_each_week
+    cal201101 = [
+                 [26, 27, 28, 29, 30, 31,  1],
+                 [ 2,  3,  4,  5,  6,  7,  8],
+                 [ 9, 10, 11, 12, 13, 14, 15],
+                 [16, 17, 18, 19, 20, 21, 22],
+                 [23, 24, 25, 26, 27, 28, 29],
+                 [30, 31,  1,  2,  3,  4,  5]
+                ]
     jan = Calendar.new(2011, 1)
-    count = 0
-    jan.each_week do
-      count += 1
+    result = []
+    jan.each_week do |week|
+      result << week.map { |date| date.day }
     end
-    assert_equal 6, count
-
-    apl = Calendar.new(2011, 4)
-    count = 0
-    apl.each_week do
-      count += 1
-    end
-    assert_equal 5, count
+    assert_equal cal201101, result
   end
 
+  def test_each_week_return_self
+    jan = Calendar.new(2011, 1)
+    assert_equal jan, (jan.each_week { })
+  end
+
+  def test_each_day
+    cal201101 = [
+                                          1,
+                  2,  3,  4,  5,  6,  7,  8,
+                  9, 10, 11, 12, 13, 14, 15,
+                 16, 17, 18, 19, 20, 21, 22,
+                 23, 24, 25, 26, 27, 28, 29,
+                 30, 31
+                ]
+    jan = Calendar.new(2011, 1)
+    result = []
+    jan.each_day do |date|
+      result << date.day
+    end
+    assert_equal cal201101, result
+  end
+
+  def test_each_day_return_self
+    jan = Calendar.new(2011, 1)
+    assert_equal jan, (jan.each_day { })
+  end
 end
